a4fef2484d5ff691f1c0f61beafaebecbe3b9ca4
Musab <mominmusab127@gmail.com>
2021-07-26 23:41:05 +0530

    complete portfolio

---
+++ gulp/build.js
@@ -0,0 +1,42 @@
+let gulp = require('gulp'),
+imagemin = require('gulp-imagemin'),
+del = require('del'),
+usemin = require('gulp-usemin');
+const gulpCssnano = require('gulp-cssnano');
+const rev = require('gulp-rev');
+const GulpUglify = require('gulp-uglify');
+
+
+
+
+gulp.task('deleteDocsFolder', ()=>
+{
+    return del('./docs')
+})
+
+
+gulp.task('optimizeImages', ()=>
+{
+    return gulp.src('./app/images/**/*')
+    .pipe(imagemin({
+        progressive: true,
+        interlaced: true,
+        multipass: true
+    }))
+    .pipe(gulp.dest('./docs/images'))
+
+});
+
+
+gulp.task('usemin', ()=>
+{
+    return gulp.src('./app/index.html')
+    .pipe(usemin({
+        css: [function(){ return rev()}, function(){ return gulpCssnano()}],
+        js: [function(){ return rev()}, function(){ return GulpUglify()}]
+    }))
+    .pipe(gulp.dest('./docs'))
+});
+
+
+gulp.task('build', gulp.series('deleteDocsFolder', 'optimizeImages', 'usemin'));
